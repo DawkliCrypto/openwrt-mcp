ccf90ebf34cac161ee8dec014993a370d35895bd
DawkliCrypto <94128252+DawkliCrypto@users.noreply.github.com>
2026-08-24 22:20:55 +0000

    Bump version to 0.5.16

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 //
 // Released under the MIT Licence. See the LICENSE file.
 
-var version = "0.5.15"
+var version = "0.5.16"
 
 const sourceURL = "https://github.com/GlassOnTin/openwrt-mcp"
 
